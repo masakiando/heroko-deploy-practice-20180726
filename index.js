a697978c9d5c3627aadf5d047a42354f67c8713c
Your Name <you@example.com>
2018-07-26 19:13:05 +0800

    update res.json in index.js heroku test

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ app.get('/users', function(req, res) {
   res.json([
     {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
     {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"},
+    {"id": 4,"firstName":"Ando","lastName":"Masaki","email":"hello@hotmail.com"}
   ]);
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port')); 
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
